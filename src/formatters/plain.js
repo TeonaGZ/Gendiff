2f11f3ab9902bf6403d2f2893b39360b5bbfbde4
Teona Gatsereliya <teonazg@gmail.com>
2023-08-27 20:05:49 +0300

    refactor stylish and plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,36 +7,37 @@ const stringify = (value) => {
   if (_.isString(value)) {
     return `'${value}'`;
   }
-  return value;
+  return String(value);
 };
 
 const getCurrentProperty = (node, property) => {
   if (property !== '') {
     return `${property}.${node.key}`;
   }
-  return `${String(node.key)}`;
+  return node.key;
 };
 
-const getPlainData = (diffTree) => {
-  const iter = (node, property) => {
-    const plainData = node.filter((data) => data.type !== 'unchanged')
-      .map((data) => {
-        switch (data.type) {
-          case 'nested':
-            return `${iter(data.children, getCurrentProperty(data, property))}`;
-          case 'added':
-            return `Property '${getCurrentProperty(data, property)}' was added with value: ${stringify(data.value)}`;
-          case 'deleted':
-            return `Property '${getCurrentProperty(data, property)}' was removed`;
-          case 'changed':
-            return `Property '${getCurrentProperty(data, property)}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
-          default:
-            throw new Error(`Unknown type: ${data.type}`);
-        }
-      });
-    return [...plainData].join('\n');
-  };
-  return iter(diffTree, '');
+const iter = (node, property) => {
+  const plainData = node
+    .filter((data) => data.type !== 'unchanged')
+    .map((data) => {
+      switch (data.type) {
+        case 'nested':
+          return `${iter(data.children, getCurrentProperty(data, property))}`;
+        case 'added':
+          return `Property '${getCurrentProperty(data, property)}' was added with value: ${stringify(data.value)}`;
+        case 'deleted':
+          return `Property '${getCurrentProperty(data, property)}' was removed`;
+        case 'changed':
+          return `Property '${getCurrentProperty(data, property)}' was updated. From ${stringify(data.value1)} to ${stringify(data.value2)}`;
+        default:
+          throw new Error(`Unknown type: ${data.type}`);
+      }
+    })
+    .join('\n');
+  return plainData;
 };
+
+const getPlainData = (diffTree) => iter(diffTree, '');
 
 export default getPlainData;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,45 +2,41 @@ import _ from 'lodash';
 
 const replacer = ' ';
 const spacesCount = 4;
+const addSign = '+ ';
+const deleteSign = '- ';
+const unchangedSign = '  ';
 
-const getIndent = (depth) => replacer.repeat((spacesCount * depth) - 2);
-const getBracketIndent = (depth) => replacer.repeat(spacesCount * (depth - 1));
+const getIndent = (depth) => replacer.repeat(spacesCount * depth - 2);
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
   const entriesData = Object.entries(data);
-  const lines = entriesData.map(([key, value]) => `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`);
-  return ['{', ...lines, `${getBracketIndent(depth)}}`].join('\n');
+  const lines = entriesData.map(([key, value]) => `${getIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${lines.join('\n')}\n${getIndent(depth)}  }`;
 };
 
-const getStylishData = (diffTree) => {
-  const iter = (node, depth) => {
-    const indent = getIndent(depth);
-    const newValue = '+ ';
-    const oldValue = '- ';
-    const sameValue = '  ';
+const iter = (node, depth) => node.map((data) => {
+  switch (data.type) {
+    case 'nested':
+      return `${getIndent(depth)}${unchangedSign}${data.key}: {\n${iter(data.children, depth + 1).join('')}${getIndent(depth)}  }\n`;
+    case 'added':
+      return `${getIndent(depth)}${addSign}${data.key}: ${stringify(data.value, depth)}\n`;
+    case 'deleted':
+      return `${getIndent(depth)}${deleteSign}${data.key}: ${stringify(data.value, depth)}\n`;
+    case 'changed': {
+      const line1 = `${getIndent(depth)}${deleteSign}${data.key}: ${stringify(data.value1, depth)}`;
+      const line2 = `${getIndent(depth)}${addSign}${data.key}: ${stringify(data.value2, depth)}`;
+      return `${line1}\n${line2}\n`;
+    }
+    case 'unchanged':
+      return `${getIndent(depth)}${unchangedSign}${data.key}: ${stringify(data.value, depth)}\n`;
+    default:
+      throw new Error(`Unknown type: ${data.type}`);
+  }
+});
 
-    const stylishData = node.map((data) => {
-      switch (data.type) {
-        case 'nested':
-          return `${indent}${sameValue}${data.key}: ${iter(data.children, depth + 1)}`;
-        case 'added':
-          return `${indent}${newValue}${data.key}: ${stringify(data.value, depth + 1)}`;
-        case 'deleted':
-          return `${indent}${oldValue}${data.key}: ${stringify(data.value, depth + 1)}`;
-        case 'changed':
-          return `${indent}${oldValue}${data.key}: ${stringify(data.value1, depth + 1)}\n${indent}${newValue}${data.key}: ${stringify(data.value2, depth + 1)}`;
-        case 'unchanged':
-          return `${indent}${sameValue}${data.key}: ${stringify(data.value, depth + 1)}`;
-        default:
-          throw new Error(`Unknown type: ${data.type}`);
-      }
-    });
-    return `{\n${stylishData.join('\n')}\n${getBracketIndent(depth)}}`;
-  };
-  return iter(diffTree, 1);
-};
+const getStylishData = (diffTree) => `{\n${iter(diffTree, 1).join('')}}`;
 
 export default getStylishData;
